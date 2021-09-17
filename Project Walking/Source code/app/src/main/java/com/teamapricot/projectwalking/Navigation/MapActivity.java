56f12f2459a671efb252b272853dbfdef0b8a180
jmollyj <jmolly@student.chalmers.se>
2021-09-17 18:06:34 +0200

    MapActivity
    started layout of map

---
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/Navigation/MapActivity.java
@@ -0,0 +1,16 @@
+package com.teamapricot.projectwalking.Navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.teamapricot.projectwalking.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
