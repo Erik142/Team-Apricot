1cabd78bccf7a09eeb76d2fe1adbeacd4da41d5b
Erik Wahlberger <EWahlberger@gmail.com>
2021-09-18 18:39:12 +0200

    Add photo capture functionality
    Adds a FloatingActionButton in the GUI used for capturing photos with the device's camera. Adds logic to start the photo capture process in MainActivity

--- Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/MainActivity.java
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/MainActivity.java
@@ -3,12 +3,27 @@ package com.teamapricot.projectwalking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.teamapricot.projectwalking.handlers.CameraHandler;
+import com.teamapricot.projectwalking.photos.PhotoController;
 
 public class MainActivity extends AppCompatActivity {
+    private PhotoController photoController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        PermissionHandler permissionHandler = new PermissionHandler(this);
+        CameraHandler cameraHandler = new CameraHandler(this);
+
+        photoController = new PhotoController(this, permissionHandler, cameraHandler);
+
         setContentView(R.layout.activity_main);
+    }
+
+    public void captureImage(View view) {
+        photoController.captureImageAsync();
     }
 }
