692efc76109e523f9c961d86a7fe318191d281a6
Daniel Brännvall <daniel@brannvall.net>
2021-09-19 17:22:38 +0200

    Added LocationHandler

--- Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/MainActivity.java
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/MainActivity.java
@@ -3,12 +3,21 @@ package com.teamapricot.projectwalking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    LocationHandler locationHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationHandler = new LocationHandler(this, 2000);
+
+        locationHandler.registerUpdateListener(() -> {
+            ((TextView)findViewById(R.id.helloWorld))
+                    .setText("(" + locationHandler.getLatitude() + "," + locationHandler.getLongitude() + ")");
+        });
     }
 }
