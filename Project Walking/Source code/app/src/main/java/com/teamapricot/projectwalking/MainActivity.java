4d5d1f6ebb18e5bc7505a6f116c7920b91332da1
Erik Wahlberger <EWahlberger@gmail.com>
2021-09-20 16:42:30 +0200

    Import TextView
    It disappeared when merging the master branch with the current branch.

--- Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/MainActivity.java
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.teamapricot.projectwalking.handlers.CameraHandler;
 import com.teamapricot.projectwalking.photos.PhotoController;
