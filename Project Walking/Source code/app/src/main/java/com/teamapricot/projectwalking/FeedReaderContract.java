ea433e481840c75637defa50891f28d65ae4c343
jmollyj <jmolly@student.chalmers.se>
2021-09-27 15:52:52 +0200

    added code for FeedReaderContract

--- Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/FeedReaderContract.java
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/FeedReaderContract.java
@@ -1,2 +1,121 @@
-package com.teamapricot.projectwalking;public class FeedReaderContract {
+package com.teamapricot.projectwalking;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+import java.util.*;
+
+
+public final class FeedReaderContract {
+
+    // Contains constants that defines names for URI's, tables, and columns.
+    // Allows you to use the same constants across all the other classes in the same package
+    // Class to prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+
+    public Context context;
+
+
+    private FeedReaderContract() {}
+
+    /* Inner class that defines the table contents */
+    public static class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+    }
+
+    //Methods for create and maintain the database and table
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
+                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                    FeedEntry.COLUMN_NAME_TITLE + " TEXT," +
+                    FeedEntry.COLUMN_NAME_SUBTITLE + " TEXT)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+
+
+
+    // Sub-class for overrides onCreate() and onUpgrade() callback methods of the database, as well as adding methods onDowngrade() and onOpen()
+    public class FeedReaderDbHelper extends SQLiteOpenHelper {
+
+
+
+        // If you change the database schema, you must increment the database version.
+        public static final int DATABASE_VERSION = 1;
+        public static final String DATABASE_NAME = "FeedReader.db";
+
+        public FeedReaderDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_ENTRIES);
+        }
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // This database is only a cache for online data, so its upgrade policy is
+            // to simply to discard the data and start over
+            db.execSQL(SQL_DELETE_ENTRIES);
+            onCreate(db);
+        }
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
+
+    }
+
+        // instantiate the FeedReaderDbHelper class
+        FeedReaderDbHelper dbHelper = new FeedReaderDbHelper(context.getApplicationContext());
+
+
+        /* Put information into a database */
+        // Gets the data repository in write mode
+//        SQLiteDatabase db = dbHelper.getWritableDatabase();
+//
+//        // Create a new map of values, where column names are the keys
+//        ContentValues values = new ContentValues();
+//        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, title);
+//        values.put(FeedEntry.COLUMN_NAME_SUBTITLE, subtitle);
+//
+//        // Insert the new row, returning the primary key value of the new row
+//        long newRowId = db.insert(FeedEntry.TABLE_NAME, null, values);
+//
+//
+//
+//        /*Read information from a database*/
+//        SQLiteDatabase db = dbHelper.getReadableDatabase();
+//
+//        // Define a projection that specifies which columns from the database
+//        // you will actually use after this query.
+//        String[] projection = {
+//            BaseColumns._ID,
+//            FeedEntry.COLUMN_NAME_TITLE,
+//            FeedEntry.COLUMN_NAME_SUBTITLE
+//        };
+//
+//        // Filter results WHERE "title" = 'My Title'
+//        String selection = FeedEntry.COLUMN_NAME_TITLE + " = ?";
+//        String[] selectionArgs = { "My Title" };
+//
+//        // How you want the results sorted in the resulting Cursor
+//        String sortOrder =
+//            FeedEntry.COLUMN_NAME_SUBTITLE + " DESC";
+//
+//        Cursor cursor = db.query(
+//            FeedEntry.TABLE_NAME,   // The table to query
+//            projection,             // The array of columns to return (pass null to get all)
+//            selection,              // The columns for the WHERE clause
+//            selectionArgs,          // The values for the WHERE clause
+//            null,                   // don't group the rows
+//            null,                   // don't filter by row groups
+//            sortOrder               // The sort order
+//    );
+
+
+
+
 }
+
