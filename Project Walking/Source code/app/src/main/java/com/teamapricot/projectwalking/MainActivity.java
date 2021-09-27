d5411941d61e66e86fdc3d0dc51093d140efe137
valeria9090 <74568062+valeria9090@users.noreply.github.com>
2021-09-27 08:48:46 +0200

    Update MainActivity.java
    removed notify button since the notification is triggered by another event.

--- Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/MainActivity.java
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/MainActivity.java
@@ -4,35 +4,27 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button notifyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         createChannel();
-        notifyBtn = findViewById(R.id.notify_button);
-
-        //button displaying notification when clicked
-        notifyBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ////using the notification method from reminder class.
-                Reminder GetNotified = new Reminder(MainActivity.this);
-                GetNotified.addNotification("notify_btn", "notice", "notify_button",1); }
-        });
+        Reminder GetNotified = new Reminder(MainActivity.this);
+        GetNotified.addNotification("new_spot", "new_challenge", "notify_button", 1);
     }
-    //creating the notification_channel for higher versions
+
+    /**
+     * description: creating the notification_channel for higher versions
+     */
     public void createChannel(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel("notify_button", "notify_btn", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationChannel channel = new NotificationChannel("notify_button", "new_spot", NotificationManager.IMPORTANCE_DEFAULT);
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
         }
