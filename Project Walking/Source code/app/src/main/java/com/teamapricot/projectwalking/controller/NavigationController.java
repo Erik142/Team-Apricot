583975445e278f89f5a47fb2c0004fb36946eaf2
Erik Wahlberger <EWahlberger@gmail.com>
2021-09-28 18:52:52 +0200

    Add javadoc to NavigationController

--- Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/controller/NavigationController.java
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/controller/NavigationController.java
@@ -8,6 +8,12 @@ import com.teamapricot.projectwalking.observe.Observer;
 
 import org.osmdroid.util.GeoPoint;
 
+/**
+ * @author Erik Wahlberger
+ * @version 2021-09-28
+ *
+ * Controller class for navigation functionality
+ */
 public class NavigationController {
     private final int LOCATION_UPDATE_INTERVAL_MS = 2000;
     private final double INITIAL_ZOOM_LEVEL = 19.5;
@@ -16,11 +22,18 @@ public class NavigationController {
     private LocationHandler locationHandler;
     private NavigationModel navigationModel;
 
+    /**
+     * Creates a new instance of the {@code NavigationController class} for the specified {@code AppCompatActivity}.
+     * @param activity The {@code AppCompatActivity} that will be used for activity results
+     */
     public NavigationController(AppCompatActivity activity) {
         this.navigationModel = new NavigationModel();
         this.activity = activity;
     }
 
+    /**
+     * Starts to update the user position, and sets the initial zoom level
+     */
     public void start() {
         locationHandler = new LocationHandler(this.activity, LOCATION_UPDATE_INTERVAL_MS);
 
@@ -32,6 +45,10 @@ public class NavigationController {
         this.navigationModel.setZoomLevel(INITIAL_ZOOM_LEVEL);
     }
 
+    /**
+     * Registers an {@code Observer} in the model.
+     * @param observer The {@code Observer<NavigationModel>} to be registered
+     */
     public void registerObserver(Observer<NavigationModel> observer) {
         this.navigationModel.addObserver(observer);
     }
