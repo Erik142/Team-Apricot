09711070e9dc21e196f68c4443ac7db2dde0e45d
Erik Wahlberger <EWahlberger@gmail.com>
2021-10-01 20:53:30 +0200

    Add more config parameters

--- Project Walking/review-police/src/interfaces/Config.ts
+++ Project Walking/review-police/src/interfaces/Config.ts
@@ -1,7 +1,12 @@
 export interface Config {
     discordToken: string,
+    discordChannelId: string,
     discordClientId: string,
     discordGuildId: string,
     gitHubPrivateRSAKey: string,
-    gitHubInstallationId: string
+    gitHubInstallationId: string,
+    gitHubSecret: string,
+    gitHubProductOwner: string,
+    smeeIoUrl: string,
+    appMode: GitHubWebHookMode
 }
