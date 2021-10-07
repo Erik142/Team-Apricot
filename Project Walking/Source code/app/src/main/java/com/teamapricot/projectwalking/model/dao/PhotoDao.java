b0183bb2e3d1fa6d66377f03512a4eed11359ba8
Erik Wahlberger <EWahlberger@gmail.com>
2021-10-07 21:09:29 +0200

    Add initial DAO for Photos

---
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/model/dao/PhotoDao.java
@@ -0,0 +1,17 @@
+package com.teamapricot.projectwalking.model.dao;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.teamapricot.projectwalking.model.Photo;
+
+import java.util.List;
+
+@Dao
+public interface PhotoDao {
+    @Query("SELECT * FROM Photos")
+    List<Photo> getAllPhotos();
+    @Insert
+    void insertAll(Photo... photos);
+}
