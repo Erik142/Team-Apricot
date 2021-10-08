4f7d750675255a42c542cd59bd3f14091e6cbdb1
Erik Wahlberger <EWahlberger@gmail.com>
2021-10-08 10:04:57 +0200

    Revert "Merge branch '26.-add-button-to-show-destn'"
    This reverts commit 110b7ef5b79b71b2fde8b1813e48d0568eb60082, reversing
    changes made to 501b562ef76db2c0f2924af57f33282d548966fb.

--- Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/controller/NavigationController.java
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/controller/NavigationController.java
@@ -1,10 +1,7 @@
 package com.teamapricot.projectwalking.controller;
 
-import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.teamapricot.projectwalking.R;
 import com.teamapricot.projectwalking.handlers.LocationHandler;
 import com.teamapricot.projectwalking.model.NavigationModel;
 import com.teamapricot.projectwalking.observe.Observer;
@@ -15,7 +12,7 @@ import org.osmdroid.util.GeoPoint;
 
 /**
  * @author Erik Wahlberger
- * @version 2021-09-28ä
+ * @version 2021-09-28
  *
  * Controller class for navigation functionality
  */
@@ -26,7 +23,6 @@ public class NavigationController {
     private AppCompatActivity activity;
     private LocationHandler locationHandler;
     private NavigationModel navigationModel;
-    private RoadManager roadManager;
 
     private boolean isOnetimeExecutionDone = false;
 
@@ -37,7 +33,6 @@ public class NavigationController {
     public NavigationController(AppCompatActivity activity) {
         this.navigationModel = new NavigationModel();
         this.activity = activity;
-        roadManager = new OSRMRoadManager(activity, "Fun Walking");
     }
 
     /**
@@ -51,6 +46,10 @@ public class NavigationController {
 
             if(!isOnetimeExecutionDone) {
                 navigationModel.setUserLocation(point);
+
+                RoadManager roadManager = new OSRMRoadManager(activity, "Fun Walking");
+                navigationModel.createDestination(roadManager);
+
                 isOnetimeExecutionDone = true;
             }
 
@@ -65,19 +64,5 @@ public class NavigationController {
      */
     public void registerObserver(Observer<NavigationModel> observer) {
         this.navigationModel.addObserver(observer);
-    }
-
-    /**
-     * registers a button for triggering new destination
-     * @param button triggers new destination
-     */
-    public void registerOnClickListener(View button) {
-        if (!button.hasOnClickListeners()) {
-            button.setOnClickListener(view -> {
-                if (view.getId() == R.id.view_dest) {
-                    this.navigationModel.createDestination(roadManager);
-                }
-            });
-        }
     }
 }
