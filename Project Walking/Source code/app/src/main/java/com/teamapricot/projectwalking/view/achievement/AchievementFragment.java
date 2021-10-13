0d973f1158a9d476f925f3be3f56d2d8b447aec4
Erik Wahlberger <EWahlberger@gmail.com>
2021-10-13 14:41:36 +0200

    Remove sample achievements data

--- Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/view/achievement/AchievementFragment.java
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/view/achievement/AchievementFragment.java
@@ -87,14 +87,6 @@ public class AchievementFragment extends Fragment {
                 PhotoDao photoDao = database.photoDao();
                 RouteDao routeDao = database.routeDao();
                 AchievementDao achievementDao = database.achievementDao();
-                routeDao.insertAll(new Route("1", 1, 1, 2, 2, 2));
-                achievementDao.insertAchievements(
-                        new Achievement("1", "Achievement 1", "Description for achievement 1", false, 1, 1, 100, 1),
-                        new Achievement("2", "Achievement 2", "Description for achievement 2", false, 1, 1, 100, 1),
-                        new Achievement("3", "Achievement 3", "Description for achievement 3", false, 1, 1, 100, 1),
-                        new Achievement("4", "Achievement 4", "Description for achievement 4", false, 1, 1, 100, 1),
-                        new Achievement("5", "Achievement 5", "Description for achievement 5", false, 1, 1, 100, 1)
-                );
                 board = new Board(photoDao, routeDao, achievementDao);
                 board.addObserver(createBoardObserver());
                 board.init();
