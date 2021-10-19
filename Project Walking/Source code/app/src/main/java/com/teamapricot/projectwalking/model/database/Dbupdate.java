cbb29c8d4b1e05328333ff21bb2ea81b2e07e2dc
DKWA0000 <joakim.tubring@gmail.com>
2021-10-19 18:18:29 +0200

    Initial version of achievement-updates.

---
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/model/database/Dbupdate.java
@@ -0,0 +1,36 @@
+package com.teamapricot.projectwalking.model.database;
+
+import com.teamapricot.projectwalking.model.database.dao.AchievementDao;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+
+
+/**
+ * Class for handling updates in databasetables, add methods if you need more.
+ */
+public class Dbupdate {
+
+/**
+* Method to readfile-input to update achievementboard
+*/
+    private void insertAchievementData(AchievementDao ad) throws IOException {
+        URL url = getClass().getResource("AchievementUpdate.txt");
+        assert url != null;
+        BufferedReader br = new BufferedReader(new FileReader(url.getPath()));
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            if((br.readLine().endsWith("."))){
+                continue;
+            }
+            String[] tmp = line.split(",");
+            Achievement aTmp = new Achievement(tmp[0], tmp[1], tmp[2], false,
+                    Integer.parseInt(tmp[3]), Integer.parseInt(tmp[4]),
+                    Integer.parseInt(tmp[5]), Integer.parseInt(tmp[6]));
+            ad.insertAchievements(aTmp);
+        }
+    }
+}
